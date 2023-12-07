f6d053e103a710fa85c441ba2e605f4d4f7c04ba
Alan Pramil <alanpramil7@gmail.com>
2023-12-07 14:30:25 +0530

    board creation

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import MobileSidebar from "./mobile-sidebar";
+import { FormPopover } from "@/components/form/form-popover";
 
 const Navbar = () => {
   return (
@@ -12,20 +13,24 @@ const Navbar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button
-          variant="primary"
-          className="hidden md:block h-auto py-1.5 px-2 rounded-sm"
-          size="sm"
-        >
-          Create
-        </Button>
-        <Button
-          size="sm"
-          variant="primary"
-          className="block rounded-sm md:hidden"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            variant="primary"
+            className="hidden md:block h-auto py-1.5 px-2 rounded-sm"
+            size="sm"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button
+            size="sm"
+            variant="primary"
+            className="block rounded-sm md:hidden"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -13,6 +13,9 @@ import { FormSubmit } from "./form-submit";
 import { useAction } from "@/hooks/use-action";
 import { createBoard } from "@/actions/create-board";
 import { toast } from "sonner";
+import { FormPicker } from "./form-picker";
+import { ElementRef, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -27,21 +30,25 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const router = useRouter();
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success("Board created!");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log(error);
       toast.error(error);
     },
   });
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    const image = formData.get("image") as string;
 
-    execute({ title });
+    execute({ title, image });
   };
 
   return (
@@ -56,7 +63,7 @@ export const FormPopover = ({
         <div className="text-sm text-center font-medium text-neutral-600 pb-4">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose asChild ref={closeRef}>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2"
             variant="ghost"
@@ -66,6 +73,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-2">
           <div className="space-y-2">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
